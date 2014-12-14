695fd3d7689a8843a147853c37134d8f2697193e
Tomislav <t.bukic@hotmail.com>
2014-12-14 15:49:46 +0100

    Update zad002.c
    ispravljena for-petlja da ide od m a ne od 1 kako je bilo.

--- Programiranje 1/Zadaci za prakticni kolokvij/zad002.c
+++ Programiranje 1/Zadaci za prakticni kolokvij/zad002.c
@@ -1,9 +1,9 @@
 /*
  * Zadatak 2:
- * Ka죥emo da su medusobno razli죅iti prirodni brojevi a i b prijateljski ako je zbroj svih djelitelja broja a, strogo manjih od a,
+ * Ka훲zemo da su medusobno razli훲citi prirodni brojevi a i b prijateljski ako je zbroj svih djelitelja broja a, strogo manjih od a,
  * jednak broju b, a zbroj svih djelitelja broja b, strogo manjih od b, jednak broju a.
- * Napi죛ite program koji u죅itava dva prirodna broja m i n, uz pretpostavku da je m < n, i ispisuje sve parove prijateljskih
- * brojeva (a, b), takve da je m ? a < b ? n. Svaki par treba ispisati to죅no jednom.
+ * Napi훲site program koji u훲citava dva prirodna broja m i n, uz pretpostavku da je m < n, i ispisuje sve parove prijateljskih
+ * brojeva (a, b), takve da je m ? a < b ? n. Svaki par treba ispisati to훲cno jednom.
  * Na primjer, ako je m = 100 i n = 300, onda program treba ispisati a = 220, b = 284, jer su to jedini prijateljski brojevi
  * izmedu 100 i 300. Brojevi a = 220 i b = 284 su prijateljski, jer su djelitelji broja a (strogo manji od njega): 1, 2, 4, 5, 10, 11,
  * 20, 22, 44, 55, 110 (suma im je 284), dok su djelitelji broja b (strogo manji od njega): 1, 2, 4, 71, 142 (njihova suma je 220).
@@ -24,7 +24,7 @@
  int main() {
 	int m, n, a, b, zbroj_dj_a, zbroj_dj_b;
 	scanf ("%d %d", &m, &n);
-	for (a = 1; a < n; ++a) {
+	for (a = m; a < n; ++a) {
 		zbroj_dj_a = zbroj_djelitelja(a);
 		for (b = 1; b < a; ++b) {
 			zbroj_dj_b = zbroj_djelitelja(b);
